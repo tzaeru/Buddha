f750722c7097f528dd3ed3688525f797f5e773f4
tzaeru <tzaeru@gmail.com>
2018-01-11 18:14:06 +0200

    initial

---
+++ Assets/MirrorUVSetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorUVSetter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(-8.0f, 8.0f), 0.0f);
+        transform.rotation = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Time.deltaTime * 120.0f, Time.deltaTime*120.0f, Time.deltaTime * 120.0f);
+
+        var renderer = GetComponent<Renderer>();
+        float x_comp = transform.forward.x;
+        float y_comp = transform.forward.y;
+        renderer.material.mainTextureOffset = new Vector2(-y_comp, x_comp);
+	}
+}
